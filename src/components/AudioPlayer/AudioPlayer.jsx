eaae41a9307dfd6b64c62038da78525bbcb33215
Antony Jaison <antonyjaison639@gmail.com>
2023-12-10 01:46:02 +0530

    added audio controls

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from '@/styles/player.module.css'
+import SongThumbnail from './_components/SongThumbnail'
+import Controls from './_components/Controls'
+import AudioControl from './_components/AudioControl'
+
 
 const AudioPlayer = () => {
   return (
-    <div>AudioPlayer</div>
+    <div className={styles.wrapper}>
+      <SongThumbnail/>
+      <Controls/>
+      <AudioControl/>
+    </div>
   )
 }
 
---
+++ src/components/AudioPlayer/_components/AudioControl.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "@/styles/controls.module.css";
+import { Volume2, VolumeX, Mic2 } from "lucide-react";
+
+const AudioControl = () => {
+  const [volume, setVolume] = useState(1);
+  const [muted, setMuted] = useState(false);
+  const finalVolume = muted ? 0 : volume ** 2;
+  return (
+    <div className={styles.volume_bar_container}>
+      <button>
+        <Mic2 width={20} />
+      </button>
+      <button onClick={() => setMuted((m) => !m)}>
+        {muted ? <VolumeX /> : <Volume2 />}
+      </button>
+      <input
+        type="range"
+        min={0}
+        max={1}
+        step={0.02}
+        value={volume}
+        onChange={(event) => {
+          setVolume(event.target.valueAsNumber);
+        }}
+      />
+    </div>
+  );
+};
+
+export default AudioControl;
