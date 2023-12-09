cf0242553305df38dfd8c89804e9155d711680c9
Antony Jaison <antonyjaison639@gmail.com>
2023-12-09 20:54:44 +0530

    added sidebar

--- src/app/(authenticated)/layout.jsx
+++ src/app/(authenticated)/layout.jsx
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 
 
@@ -5,8 +6,8 @@ export default async function MainLayout({ children }) {
   return (
    <div className="h-full flex flex-col">
     <div className="flex-1 flex p-5 gap-3.5">
-      <div className="flex-shrink-0 bg-red-300 w-80">
-          Navbar
+      <div className="flex-shrink-0 w-80">
+          <Sidebar/>
       </div>
       <div className="bg-green-400 flex-grow">{children}</div>
     </div>
